382a5c124b17a94436930944f8f1dad0e7bc5b87
DanaKLym <bohdanaklymeko@gmail.com>
2023-11-02 14:15:01 +0200

    installed and set React Router for navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import Vaccines from "./Vaccines";
+import Homepage from "./Homepage";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Gonna make some CRUD app to track vaccine injection records</h1>
+      <nav className="navbar">
+        <Link to="/" className="nav-item">
+          Homepage
+        </Link>
+        <Link to="/vaccines" className="nav-item">
+          Vaccines
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/vaccines" element={<Vaccines />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Vaccines.js
@@ -0,0 +1,9 @@
+function Vaccines() {
+  return (
+    <>
+      <h2>This is a page with Vaccines</h2>
+    </>
+  );
+}
+
+export default Vaccines;
